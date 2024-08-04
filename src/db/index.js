d6066dc8d657d84ea01f2bb672ce9c2b3579db36
Sayeem <t.kuuroo@gmail.com>
2024-08-04 17:02:18 +0530

    connect to mongoDB

---
+++ src/db/index.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js"; 
+import express from 'express'
+const app=express();
+
+
+
+const connectDB=async()=>{
+    try {
+        const connectionInstance= await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+        app.on("error",()=>{
+            console.log("ERROR!!!: ",error);
+            process.exit(1);
+        });
+        
+        console.log(`Connection success! DB Host ${connectionInstance.connection.host}`);
+        app.listen(process.env.PORT,()=>{
+         console.log(`App listening on port: ${process.env.PORT}`);
+
+        });
+         
+    } catch (error) {
+        console.log("ERROR!!: ",error)
+    }
+} 
+
+export default connectDB
